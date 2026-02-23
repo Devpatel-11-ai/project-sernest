f19b8c82681e07d0ce98f8c32ea44351d5b7510a
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-24 00:09:37 +0530

    first commit

---
+++ sernest/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from .forms import UserSignupForm
+
+# Create your views here.
+def UserSignupView(request):
+    if request.method == "POST":
+        form = UserSignupForm(request.POST or none)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return render(request, 'core/signup.html', {'form': form})
+    else:
+        form = UserSignupForm()
+        return render(request, 'core/signup.html', {'form': form})
