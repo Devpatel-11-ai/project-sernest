a4de382efa4a54bcdc62446b64921aea85533aa9
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-26 10:37:48 +0530

    changes and implementation

--- sernest/core/views.py
+++ sernest/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
-from .forms import UserSignupForm
+from django.shortcuts import render,redirect,HttpResponse
+from .forms import UserSignupForm,UserLoginForm
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def UserSignupView(request):
@@ -7,17 +8,37 @@ def UserSignupView(request):
         form = UserSignupForm(request.POST or None)
         if form.is_valid():
             form.save()
-            return redirect('core:login')
+            return redirect('login')
         else:
             return render(request, 'core/signup.html', {'form': form})
     else:
         form = UserSignupForm()
         return render(request, 'core/signup.html', {'form': form})
         
+
 def UserLoginView(request):
-    return render(request, 'core/login.html')
+  if request.method =="POST":
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+      print(form.cleaned_data)
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+      user = authenticate(request,email=email,password=password) #it will check in database..
+      if user:
+        login(request,user)
+        if user.role == 'admin':
+          return redirect('admin:index')
+        else:
+          return redirect('home')
+      else:
+        return render(request,'core/login.html',{'form':form})
+  else:
+    form = UserLoginForm()
+    return render(request,'core/login.html',{'form':form})
 
 def home(request):
     return render(request, 'home.html')
 
+def provider_register(request):
+    return render(request, 'provider_register.html')
 
